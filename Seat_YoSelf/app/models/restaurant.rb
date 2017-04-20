8353e96b16aa735e222f683b2bec258c23dbeaa2
EDWIN AU <edwinau@rogers.com>
2017-04-20 16:24:09 -0400

    write model associations, install bcrypt gem for passwords

--- Seat_YoSelf/app/models/restaurant.rb
+++ Seat_YoSelf/app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :cuisines
+  has_many :reservations
+  has_many :users, through: :reservations
+  # belongs_to :user
 end
--- Seat_YoSelf/app/models/user.rb
+++ Seat_YoSelf/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :reservations
+  has_many :restaurants, through: :reservations
+  # has_many :owned_restaurants, class_name: "Restaurant"
 end
