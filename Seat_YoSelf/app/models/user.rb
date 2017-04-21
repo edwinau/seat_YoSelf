eeab829edee21d30e156d7961e1ba92d226d4b86
Mark <markjconlon@gmail.com>
2017-04-21 16:16:46 -0400

    added validations for name username password for presence

--- Seat_YoSelf/app/models/user.rb
+++ Seat_YoSelf/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :restaurants, through: :reservations
   # has_many :owned_restaurants, class_name: "Restaurant"
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
 end
