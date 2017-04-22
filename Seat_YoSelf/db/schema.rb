2103b9fcfc9c580ca37f64402d8449f55d143bd2
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-22 12:47:35 -0400

    Removing cuisine_id from Restaurant model

--- Seat_YoSelf/db/schema.rb
+++ Seat_YoSelf/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421203112) do
+ActiveRecord::Schema.define(version: 20170422164459) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string   "cuisine_type"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20170421203112) do
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.integer  "cuisine_id"
     t.integer  "owner_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
