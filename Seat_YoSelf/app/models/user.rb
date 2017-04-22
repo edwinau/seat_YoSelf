663d04f3f3eddfec1445bb3f8c2ea2aaf0fad74b
Mark <markjconlon@gmail.com>
2017-04-22 12:09:00 -0400

    added uniqueness to User model on username

--- Seat_YoSelf/app/models/user.rb
+++ Seat_YoSelf/app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :restaurants, through: :reservations
   # has_many :owned_restaurants, class_name: "Restaurant"
   validates :name, presence: true
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 end
