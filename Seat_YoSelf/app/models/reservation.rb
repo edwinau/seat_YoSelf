9d430c007b0b80c6b5a136b8d2e6ac7f72f80df1
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-24 11:10:07 -0400

    Add capacity to Restaurant form and test validation of capacity for a given time

--- Seat_YoSelf/app/models/reservation.rb
+++ Seat_YoSelf/app/models/reservation.rb
@@ -6,12 +6,30 @@ class Reservation < ApplicationRecord
   # validates :date, presence: true
   validate :reservation_date_cannot_be_in_the_past
   validates :number_of_guests, numericality: { only_integer: true, less_than_or_equal_to: 20, greater_than_or_equal_to: 2, message: "Reservation must be made for a party of 2-20" }
+  validate :party_size_less_than_capacity
 
   def reservation_date_cannot_be_in_the_past
     if time < Time.now
       errors.add(:time, "can't be in the past")
     end
   end
+
+
+
+def party_size_less_than_capacity
+  #calculate number of guests at the at restaurant at the given time
+  current_guests = Reservation.where(time: time, restaurant_id: restaurant_id).sum(number_of_guests)
+  #find total capacity for the restaurant
+  total_capacity = Restaurant.find(restaurant_id).capacity
+
+
+  #calculate available capacity
+  available_capacity = (total_capacity - current_guests)
+
+  if number_of_guests > available_capacity
+    errors.add(:number_of_guests, "Not enough capacity for this number of guests at this time")
+  end
+end
 
 
 end
